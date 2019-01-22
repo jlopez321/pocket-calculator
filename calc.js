8dcf41c526bd5edb7095167c402b8ba55a829d73
Justice Lopez <silverheartz123@gmail.com>
2019-01-22 10:52:00 -0500

    s

--- calc.js
+++ calc.js
@@ -1,19 +1,56 @@
-var numberOfNumbers = 0
+var numberOfNumbers = 0;
+var clearDat = false;
+var numberArray = [];
+var symbolArray = [];
+var tmpNumStr = [];
+var pos = 0
 function num(number) {
   if (numberOfNumbers < 9){
-    document.getElementById('displayscreen').value = document.getElementById('displayscreen').value + number;
-    numberOfNumbers++
+    if (clearDat){
+      numberOfNumbers++;
+      clearDat = false;
+      tmpNumStr.push(number)
+    } else {
+      numberOfNumbers++
+      tmpNumStr.push(number)
+      }
+    tmps = tmpNumStr.join('')
+    numberArray[pos] = tmps;
+    pushToDis();
   }
 }
+function plusOrminus(){
+  numberArray[pos] = (numberArray[pos] * -1)
+  pushToDis()
+}
 function oper(symbol){
-  document.getElementById('displayscreen').value = document.getElementById('displayscreen').value + symbol;
+  symbolArray.push(symbol);
   numberOfNumbers = 0;
+  pos++
+  tmpNumStr = []
+  pushToDis()
 }
 function equals(){
-  let answer = eval(document.getElementById('displayscreen').value)
+  let answer = eval(document.getElementById('displayscreen').value);
+  clearDat = true;
   document.getElementById('displayscreen').value = answer
 }
 function clears(){
-  document.getElementById('displayscreen').value = 0;
+  clearDat = true;
   numberOfNumbers = 0;
+}
+function pushToDis(){
+  let outStr = [];
+  let numNum = 0
+  let symNum = 1
+  for (nums in numberArray){
+    outStr[numNum] = numberArray[nums].toString();
+    numNum += 2;
+  }
+  for (syms in symbolArray){
+    outStr[symNum] = symbolArray[syms].toString();
+    numNum += 2;
+  }
+  finalStr = outStr.join('');
+  document.getElementById('displayscreen').value = finalStr;
 }
